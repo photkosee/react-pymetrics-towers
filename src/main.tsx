9869fde8fa997c4d17559eef453f7c8002c2aaf9
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-12-03 06:06:25 +1100

    add: init nextui

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client'
+import { NextUIProvider } from '@nextui-org/react'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <NextUIProvider>
+    <App />
+  </NextUIProvider>
 )
